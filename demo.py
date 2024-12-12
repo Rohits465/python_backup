0480fd23ac59d2b235d7a609ff8eb78b255a1a9c
Rohits465 <rs7833802@gmail.com>
2024-12-12 11:04:13 +0530

    my first commit

---
+++ demo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def add(x,y):
+    z = x + y
+    return z
+
+
+# In[ ]:
+
+
+x = [11,112,3,44]
+
+
+# In[ ]:
+
+
+c = {'a':11,'b':22,'c':33}
+
